dfb2e8659cddeaa5f550d94e6aee22a5422fa865
aminul sujon <aminul532sujon@gmail.com>
2026-03-13 01:00:08 +0600

    chore: header 2 implementation

--- resources/views/news/com_h2.blade.php
+++ resources/views/news/com_h2.blade.php
@@ -1,24 +1,13 @@
 <div class="header-mid d-none d-md-block">
                    <div class="container">
                         <div class="row d-flex align-items-center">
-                            <!-- Logo -->
-                            <div class="col-xl-3 col-lg-3 col-md-3">
-                                <div class="logo py-4-">
-                                    <a href="{{ $websettings['cms_url'] }}"><img src="images/logo.jpg" alt="logo" width="71">
-                                      <span class="d-none">{{ $websettings['cms_sitename'] }}</span>
-                                    </a>
-                                </div>
-                            </div>
                             
-                            <div class="col-3">
 
-                            </div>
-
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                               <div class="row">
                                 @if(!empty($spotlight) && count($spotlight->contents) > 0)
                                 
-                                  @foreach($spotlight->contents->take(2) as $content)
+                                  @foreach($spotlight->contents->take(4) as $content)
                                     <div class="col-6">
                                       <div class="media">
                                         <a href="{{ $content->slug }}">
